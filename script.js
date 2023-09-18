16928ef7d814e148d2f000d5362570d7a219fa2d
ituq <vincentluisfranz@gmail.com>
2023-09-18 15:33:17 +0200

    fixed java script not working as intended

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-draw(16);
+const body = document.querySelector("#top")
+for(let i=1;i<=16;i++){
+    let row = document.createElement("div");
+    row.classList.add("row")
+    body.appendChild(row);
+    for(let j=0;j<16;j++){
+        let cell=document.createElement("div");
+        cell.id=`${i} ${j}`;
+        cell.addEventListener("mouseover", () => cell.style.backgroundColor="black")
+        row.appendChild(cell)
+    }
+}   
 const btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     let resolution = Number(prompt("Resolution (one axis)?"));
